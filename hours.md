bcd9c28bd29e29e08630ad28c5bd9744d4d5a515
Chad Samuelson <crsamuelson87@gmail.com>
2023-11-29 15:01:07 -0700

    still debugging rectification

--- hours.md
+++ hours.md
@@ -17,5 +17,6 @@
 | 11/25 | Stereo View Algorithm/Method to build a Dense Depth Map, watched 4.3-4.5 lecture (cont.). Outlined Densification plans. | 4 |
 | 11/27 | Writing code to undistort images | 1 |
 | 11/28 | Writing code to undistort and rectify images | 3 |
-|| **Total hours worked thus far** | **31** |
-|| **Hours Remaining out of 80  hours required** | **49** |
+| 11/29 | Fixing rectification algorithm | 1.75 |
+|| **Total hours worked thus far** | **32.75** |
+|| **Hours Remaining out of 80  hours required** | **47.25** |
--- rectify_imgs.py
+++ rectify_imgs.py
@@ -29,6 +29,17 @@ def read_args():
     parser.add_argument('--right_img_path',type=str,default="r",help='right image path name (as appears in reconstruction file')  
     return parser
 
+def get_cam_intrinsics(camera_params):
+    f = camera_params["focal"]
+    cx = camera_params["width"]//2
+    cy = camera_params["height"]//2
+    K = np.array([
+        [f,0,cx],
+        [0,f,cy],
+        [0,0,1]
+    ])
+    return K
+
 if __name__ == '__main__':
     ## Load Data
     args = vars(read_args().parse_args())
@@ -36,8 +47,11 @@ if __name__ == '__main__':
         reconstruction_data = json.load(file)[0]
     cal_file = open(args['cam_cal'],'rb')
     K, dist_coeff = pkl.load(cal_file)
-    Kinv = np.linalg.inv(K)
     cal_file.close()
+    # camera_params = list(reconstruction_data["cameras"].values())[0]
+    # K = get_cam_intrinsics(camera_params)
+    # dist_coeff = np.array([camera_params['k1'],camera_params['k2'],0,0])
+    Kinv = np.linalg.inv(K)
 
     ## Load left & right image
     l_img = cv2.imread(args['left_img_path'])
@@ -46,58 +60,111 @@ if __name__ == '__main__':
 
     ## Estimate Essential Matrix
     l_img_name = os.path.basename(args['left_img_path'])
+    # l_img_name = l_img_name[12:] # remove "undistorted_" naming
     r_img_name = os.path.basename(args['right_img_path'])
+    # r_img_name = r_img_name[12:] # remove "undistorted_" naming
     for img_name, img_data in reconstruction_data["shots"].items():
         if img_name == l_img_name:
-            Rl = rot_mat_from_axisangle(img_data["rotation"])
+            Rl, _ = cv2.Rodrigues(np.array(img_data["rotation"]))#rot_mat_from_axisangle(img_data["rotation"])
             tl = np.array(img_data["translation"])
         elif img_name == r_img_name:
-            Rr = rot_mat_from_axisangle(img_data["rotation"])
+            Rr, _ = cv2.Rodrigues(np.array(img_data["rotation"]))#rot_mat_from_axisangle(img_data["rotation"])
             tr = np.array(img_data["translation"])
-    R = Rr - Rl
-    t = tr - tl
+    
 
-    # Construct Rrect
-    print("Constructing R_rect Matrix...")
-    r1 = t / np.linalg.norm(t,2)
-    r2 = np.array([[0,-1,0],[1,0,0],[0,0,0]]) @ r1
-    r3 = np.cross(r1,r2)
-    Rrect = np.vstack((r1,r2,r3))
-    assert((3,3) == Rrect.shape)
+    # E = np.dot(np.linalg.inv(Rr),tr-tl)
+    # Hl = Rl + np.dot(tl,E.T)
+    # Hr = Rr + np.dot(tr,E.T)
+    # # Rectify the images using the homography matrices
+    # l_img_rect = cv2.warpPerspective(l_img, Hl, (l_img.shape[1], l_img.shape[0]))
+    # r_img_rect = cv2.warpPerspective(r_img, Hr, (r_img.shape[1], r_img.shape[0]))
 
-    # Warp Pixels into new rectified frames
-    print("Warping Pixels into rectified coordinates...")
-    l_rect_pix = []
-    r_rect_pix = []
-    for row in range(l_img.shape[0]):
-        for col in range(l_img.shape[1]):
-            l_pix = K @ Rrect @ Kinv @ np.array([[col,row,1]]).T # [x,y,1]
-            l_rect_pix.append(l_pix[:,0])
+
+    # Construct the projection matrices
+    P_left = np.hstack((Rl, tl.reshape(3, 1)))
+    P_right = np.hstack((Rr, tr.reshape(3, 1)))
+
+    # Compute the rectification transformations
+    R = Rr.T @ Rl
+    T = tr - tl
+
+    r1,r2,p1,p2,q,roi1,roi2 = cv2.stereoRectify(
+        K, dist_coeff, K, dist_coeff, l_img.shape[:2], R, T
+    )
+
+    # Apply rectification to the images
+    map_left_x, map_left_y = cv2.initUndistortRectifyMap(
+        K, 
+        dist_coeff, 
+        R=r2, 
+        newCameraMatrix=p2, 
+        size=l_img.shape[:2], 
+        m1type=cv2.CV_32FC1)
+    map_right_x, map_right_y = cv2.initUndistortRectifyMap(
+        K, 
+        dist_coeff, 
+        R=r1, 
+        newCameraMatrix=p1, 
+        size=r_img.shape[:2], 
+        m1type=cv2.CV_32FC1)
+
+    l_img_rect = cv2.remap(l_img, map_left_x, map_left_y, cv2.INTER_LINEAR)
+    r_img_rect = cv2.remap(r_img, map_right_x, map_right_y, cv2.INTER_LINEAR)
+
+    
+    # R = Rr.T @ Rl
+    # t = tr - tl
+
+    # # # Rotate Right image by applying the affine transformation to the image
+    # # r_img_rot = cv2.warpAffine(r_img, R, (r_img.shape[1], r_img.shape[0]))
+
+    # # Construct Rrect
+    # print("Constructing R_rect Matrix...")
+    # r1 = t / np.linalg.norm(t,2)
+    # r2 = np.array([[0,-1,0],[1,0,0],[0,0,0]]) @ r1
+    # r3 = np.cross(r1,r2)
+    # Rrect = np.vstack((r1,r2,r3))
+    # assert((3,3) == Rrect.shape)
+
+
+    # # Rotate Right image by applying the affine transformation to the image
+    # r_img_rect = cv2.warpPerspective(r_img, R@Rrect, (r_img.shape[1], r_img.shape[0]))
+    # l_img_rect = cv2.warpPerspective(l_img, Rrect, (l_img.shape[1], l_img.shape[0]))
+
+
+    # # Warp Pixels into new rectified frames
+    # print("Warping Pixels into rectified coordinates...")
+    # l_rect_pix = []
+    # r_rect_pix = []
+    # for row in range(l_img.shape[0]):
+    #     for col in range(l_img.shape[1]):
+    #         l_pix = K @ Rrect @ Kinv @ np.array([[col,row,1]]).T # [x,y,1]
+    #         l_rect_pix.append(l_pix[:,0])
             
-            r_pix = K @ R @ Rrect @ Kinv @ np.array([[col,row,1]]).T # [x,y,1]
-            r_rect_pix.append(r_pix[:,0])
+    #         r_pix = K @ R @ Rrect @ Kinv @ np.array([[col,row,1]]).T # [x,y,1]
+    #         r_rect_pix.append(r_pix[:,0])
 
-    print("Warping rectified pixels into new rectified frames...")
-    l_rect_img = np.zeros_like(l_img)
-    r_rect_img = np.zeros_like(r_img)
-    idx = 0
-    for row in range(l_img.shape[0]):
-        for col in range(l_img.shape[1]):
-            l_pix = l_rect_pix[idx].astype(int) 
-            if (l_pix[0] >= 0) and (l_pix[0] <= l_img.shape[1]) and \
-                (l_pix[1] >= 0) and (l_pix[1] <= l_img.shape[0]):
+    # print("Warping rectified pixels into new rectified frames...")
+    # l_rect_img = np.zeros_like(l_img)
+    # r_rect_img = np.zeros_like(r_img)
+    # idx = 0
+    # for row in range(l_img.shape[0]):
+    #     for col in range(l_img.shape[1]):
+    #         l_pix = l_rect_pix[idx].astype(int) 
+    #         if (l_pix[0] >= 0) and (l_pix[0] <= l_img.shape[1]) and \
+    #             (l_pix[1] >= 0) and (l_pix[1] <= l_img.shape[0]):
 
-                # print(f"row: {row}, col: {col}, l_pix: {l_pix}, l_rect_img.shape: {l_rect_img.shape}, l_img.shape: {l_img.shape}")
-                l_rect_img[row,col,:] = l_img[l_pix[1], l_pix[0], :]
+    #             print(f"row: {row}, col: {col}, l_pix: {l_pix}, l_rect_img.shape: {l_rect_img.shape}, l_img.shape: {l_img.shape}")
+    #             l_rect_img[row,col,:] = l_img[l_pix[1], l_pix[0], :]
             
-            r_pix = r_rect_pix[idx].astype(int)
-            if (r_pix[0] >= 0) and (r_pix[0] <= r_img.shape[1]) and \
-                (r_pix[1] >= 0) and (r_pix[1] <= r_img.shape[0]):
+    #         r_pix = r_rect_pix[idx].astype(int)
+    #         if (r_pix[0] >= 0) and (r_pix[0] <= r_img.shape[1]) and \
+    #             (r_pix[1] >= 0) and (r_pix[1] <= r_img.shape[0]):
 
-                # print(f"row: {row}, col: {col}, r_pix: {r_pix}, r_rect_img.shape: {r_rect_img.shape}, r_img.shape: {r_img.shape}")
-                r_rect_img[row,col,:] = r_img[r_pix[1], r_pix[0], :]
+    #             print(f"row: {row}, col: {col}, r_pix: {r_pix}, r_rect_img.shape: {r_rect_img.shape}, r_img.shape: {r_img.shape}")
+    #             r_rect_img[row,col,:] = r_img[r_pix[1], r_pix[0], :]
 
-            idx += 1
+    #         idx += 1
     
     # Display Rectified Images
     cv2.namedWindow('Left Image', cv2.WINDOW_NORMAL)
@@ -105,8 +172,8 @@ if __name__ == '__main__':
     cv2.namedWindow('Right Image', cv2.WINDOW_NORMAL)
     cv2.imshow("Right Image", r_img)
     cv2.namedWindow('Left Rectified Image', cv2.WINDOW_NORMAL)
-    cv2.imshow("Left Rectified Image", l_rect_img)
+    cv2.imshow("Left Rectified Image", l_img_rect)
     cv2.namedWindow('Right Rectified Image', cv2.WINDOW_NORMAL)
-    cv2.imshow("Right Rectified Image", r_rect_img)
+    cv2.imshow("Right Rectified Image", r_img_rect)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
